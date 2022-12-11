c432e2d660700ee0dcf33fc7b08832070fd9a1c9
Felipe Sanchez <felipesanme@outlook.com>
2022-12-11 12:14:33 -0500

    code lab H2 db

---
+++ codelabh2db/src/main/java/com/codelabh2db/codelabh2db/Codelabh2dbApplication.java
@@ -0,0 +1,13 @@
+package com.codelabh2db.codelabh2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codelabh2dbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codelabh2dbApplication.class, args);
+	}
+
+}
---
+++ codelabh2db/src/test/java/com/codelabh2db/codelabh2db/Codelabh2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelabh2db.codelabh2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codelabh2dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
